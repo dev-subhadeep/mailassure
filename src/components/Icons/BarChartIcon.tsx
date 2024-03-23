9e7b46fd29cb938c14daa5c61eae97c655de5557
Subhadeep <subhadeepsarkar1995@gmail.com>
2024-03-23 08:12:05 +0530

    feat: Sidebar UI

---
+++ src/components/Icons/BarChartIcon.tsx
@@ -0,0 +1,28 @@
+import React from "react"
+
+const BarChartIcon = () => {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M5.33342 6.28548H0.666748V19.1188H5.33342V6.28548Z"
+        fill="#919EAB"
+      />
+      <path
+        d="M19.3334 10.9521H14.6667V19.1188H19.3334V10.9521Z"
+        fill="#919EAB"
+      />
+      <path
+        d="M12.3334 0.452148H7.66675V19.1188H12.3334V0.452148Z"
+        fill="#919EAB"
+      />
+    </svg>
+  )
+}
+
+export default BarChartIcon
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image"
+import React from "react"
+import Logo from "../../public/Logo.png"
+import { Button } from "@/components/ui/button"
+import HomeIcon from "./Icons/HomeIcon"
+import EmailIcon from "./Icons/EmailIcon"
+import SendIcon from "./Icons/SendIcon"
+import ListIcon from "./Icons/ListIcon"
+import InboxIcon from "./Icons/InboxIcon"
+import BarChartIcon from "./Icons/BarChartIcon"
+import DummyUserIcon from "../../public/images/user_icon.png"
+
+const Sidebar = () => {
+  return (
+    <nav className="w-[56px] flex flex-col items-center justify-between h-screen border-r border-slate-100 px-2 py-4">
+      <div className="flex flex-col items-center">
+        <div className="mb-[100px]">
+          <Image src={Logo} height={32} width={32} alt="logo" />
+        </div>
+        <div className="flex flex-col justify-between h-[400px]">
+          <Button variant="ghost">
+            <HomeIcon />
+          </Button>
+          <Button variant="ghost">
+            <EmailIcon />
+          </Button>
+          <Button variant="ghost">
+            <SendIcon />
+          </Button>
+          <Button variant="ghost">
+            <ListIcon />
+          </Button>
+          <Button variant="ghost">
+            <InboxIcon />
+          </Button>
+          <Button variant="ghost">
+            <BarChartIcon />
+          </Button>
+        </div>
+      </div>
+      <div className="justify-self-end">
+        <Image src={DummyUserIcon} width={32} height={32} alt="User Icon" />
+      </div>
+    </nav>
+  )
+}
+
+export default Sidebar
