c9cc6a0702450c0e58345a6bcccbd0b38d2cdd81
Subhadeep <subhadeepsarkar1995@gmail.com>
2024-03-23 21:06:49 +0530

    feat: Tag now produces different colors based on type

--- src/components/tag/index.tsx
+++ src/components/tag/index.tsx
@@ -10,15 +10,34 @@ const Tag = ({ type }: TagProps) => {
     <div className="flex gap-2 min-h-1 min-w-1 items-center">
       <div
         className={cn(
-          "h-3 w-3 rounded-full",
-          "bg-green-400 bg-opacity-35 shadow-xl flex justify-center items-center"
+          "h-3 w-3 rounded-full bg-opacity-35 shadow-xl flex justify-center items-center",
+          type === "interested" && "bg-green-400",
+          type === "meeting booked" && "bg-purple-400",
+          type === "meeting completed" && "bg-yellow-400",
+          type === "closed" && "bg-indigo-400"
         )}
       >
         <div
-          className={cn("h-2 w-2 rounded-full p-1", "bg-green-400 shadow-xl")}
+          className={cn(
+            "h-2 w-2 rounded-full p-1",
+            type === "interested" && "bg-green-400",
+            type === "meeting booked" && "bg-purple-400",
+            type === "meeting completed" && "bg-yellow-400",
+            type === "closed" && "bg-indigo-400"
+          )}
         ></div>
       </div>
-      <div className="text-green-500 capitalize">{type}</div>
+      <div
+        className={cn(
+          "capitalize",
+          type === "interested" && "text-green-400",
+          type === "meeting booked" && "text-purple-400",
+          type === "meeting completed" && "text-yellow-400",
+          type === "closed" && "text-indigo-400"
+        )}
+      >
+        {type}
+      </div>
     </div>
   )
 }
