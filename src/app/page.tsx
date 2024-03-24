aa7000a6b52937d5750e98575b2f47e5a0b01a41
Subhadeep <subhadeepsarkar1995@gmail.com>
2024-03-24 09:00:49 +0530

    Revert: Changed root page back to server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,3 @@
-"use client"
-import { useSearchParams } from "next/navigation"
-
 export default function Home() {
-  const searchParams = useSearchParams()
-  const token = searchParams.get("token")
-  return (
-    <main>
-      <p>{token}</p>
-    </main>
-  )
+  return <main></main>
 }
